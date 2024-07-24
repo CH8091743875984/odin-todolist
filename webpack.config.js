ed49b67ca1aa30388355c251d597170d8af31e96
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2024-07-24 15:51:35 -0500

    Set to dev for testing purposes

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development', //change back to production when ready, to minify the names
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
