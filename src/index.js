643cb5addada60fed35b73d4d73409f8cbd19b43
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2024-07-31 10:22:45 -0500

    Clean up

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
 import {Task, Project, Portfolio} from "./functions.js";
-import {renderInitialPortfolio, renderUI, renderTask, renderProject} from "./ui.js";
+import {renderInitialPortfolio, renderUI} from "./ui.js";
 import './style.css';
 
-console.log('testing 1 2 3 4')
-
-// let testTD1 = new ToDo('Test 1 TD')
-// let testTD2 = new ToDo('Test 2 TD')
-// let testProject1 = new Project('My First Project')
-
-// testProject1.addToDo(testTD1)
-// testProject1.addToDo(testTD2)
-// // console.log(testProject1)
-
-// window.testTD1 = testTD1
-// window.testTD2 = testTD2
-// window.testProject1 = testProject1
-// window.ToDo = ToDo
+console.log('initializing')
 
 let myPortfolio = new Portfolio('myPortfolio')
 
@@ -30,10 +17,6 @@ myProject.assignTask(myTask2)
 
 window.myPortfolio = myPortfolio
 
-// testRender(testProject1)
-
-
-//now need to turn this into creating the classes and prefilling the forms.
 renderUI()
 
 renderInitialPortfolio(myPortfolio)
